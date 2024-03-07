fab7fd4a8f0e035d0774295f7065577f4a7e8699
Aaron Imbrock <aimbrock@gmail.com>
2024-03-07 09:04:12 -0600

    Broke out Button and FilterButton from Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
@@ -8,44 +10,16 @@ function App(props) {
       id={task.id}
       name={task.name}
       completed={task.completed}
-      />
-    ));
-    return (
+    />
+  ));
+  return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
@@ -53,12 +27,9 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         {taskList}
-        {/* <Todo name="Eat" completed id="todo-0" />
-        <Todo name="Sleep" id="todo-1" />
-        <Todo name="Repeat" id="todo-2" /> */}
       </ul>
     </div>
-    );
+  );
 }
 
-    export default App;
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+
+function Button(props) {
+    return (
+        <button type="button" className={props.className} onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
+}
+
+export default Button;
